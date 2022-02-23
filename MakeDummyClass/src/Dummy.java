f439b9416072c641333da961ae93cccc28322616
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-02-24 00:15:21 +0900

    change add dummy class

--- MakeDummyClass/src/Dummy.java
+++ MakeDummyClass/src/Dummy.java
@@ -1,8 +1,8 @@
 public class Dummy {
     public static void main(String[] args) {
 
-        
+        String start = "insert into tblmovielist (mlseq, title, makeyear, makecountry, type, ganre, makesituation, director, makecompany) values";
 
-
+        System.out.printf("%s", start);
     }
 }
